4bd73e60f8f6e7bef15876abb3ec96075b4638bf
Tomer Lev <tomer.l@lusha.com>
2021-07-12 22:18:56 +0300

    clarify further the brokenness of C++. why the fuck are we using C++?

--- api/routes/main.js
+++ api/routes/main.js
@@ -5,7 +5,7 @@ app.get('/api/raspan', async (req, res) => {
     const headers = {
         'preparedvisittoken': '4437e9aa-9808-4541-950d-2802cc13fb37',
         'application-api-key': '8640a12d-52a7-4c2a-afe1-4411e00e3ac4',
-        'authorization': 'JWT ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InljeDFyWFRmalRjQjZIQWV1aGxWQklZZmZUbyJ9.eyJpc3MiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJhdWQiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJuYmYiOjE2MTkzMDYxOTgsImV4cCI6MTY1MDQxMDE5OCwidW5pcXVlX25hbWUiOiJjZjBhM2E4ZS1lZmMyLTRlN2EtOTJjMS04ZTc1OTgzYTQ1YTYifQ.ZHj5wgZNrpMYzbo0toQRJXEZWUK1Uw8wRG1GAaTtHL8V4rYZr96fN4dVXq2ftqDTeYQ48jRBt4EfspxfollSc0pf4e64XGpLzsASaS-5PHCvgSg4ELJF0NtwS1j7_DmtOuCF0dyWemA4pb-DBbr8zXizqEqyO9s60crWJ5hh__bOibyIhBczjq4NZjZ-kkbbaMp014zq1SKhYUc1IYZKbNR563Yu8hq2MzHhlVRd5ycaMS7rL3rzY_50wPB6pxou10ajcchTGY6GV0LTniCCdqrMZzaxpu_TgIl1U1AlToZw1mcOmTSk0QPIjcBUF68DT9UIk2WPPunWP3rca5S1pw',
+        'authorization': 'JWT ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InljeDFyWFRmalRjQjZIQWV1aGxWQklZZmZUbyJ9.eyJpc3MiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJhdWQiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJuYmYiOjE2MTkyNTYzNjUsImV4cCI6MTY1MDM2MDM2NSwidW5pcXVlX25hbWUiOiJjZjBhM2E4ZS1lZmMyLTRlN2EtOTJjMS04ZTc1OTgzYTQ1YTYifQ.o1s-9MuI-QcuB7vlGOSDw0jwdsUgSZjUiDr2e_67BYs0SmIGfYFvITrQhZpgbqZYArVEW1jL0HxvWl8-Eq_1o69BTWFYrxeMGIAxQf7Mx4-ANkRxc9oUTiB0LOTjePzebqt1yAJaCIGWDWEsYj7Iyz-yL2cttic8QNfWyXCL6z8ovYxGmmGs9H4IK4LhQ3xXvKlOvHsP2cstAP9d50UgZ4D1AuVrgm6NfO3hd3XtF5mJiAwkUX2ry5EcUHRC57ECaVi7I76CST_skYf-93h2zzPHQ3hZ4steyF3z3a4kTlkeTn9HbwhM0rzxZW8ONtz7JrCPL1bJsYG9bNfk63dsSQ',
         'accept': 'application/json, text/plain, */*',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
         'application-name': 'myVisit.com v3.5',
@@ -20,8 +20,8 @@ app.get('/api/raspan', async (req, res) => {
     let date = ("0" + date_ob.getDate()).slice(-2);
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     let year = date_ob.getFullYear();
-    console.log(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=6142&startDate=${year + "-" + month + "-" + date}`);
-    const response = await axios.get(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=6142&startDate=${year + "-" + month + "-" + date}`, { headers: headers })
+    console.log(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=8886&startDate=${year + "-" + month + "-" + date}`);
+    const response = await axios.get(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=8886&startDate=${year + "-" + month + "-" + date}`, { headers: headers })
     const result = response.data
     if  (result['Success'] === false) {
         console.log((`Error [${result['ErrorNumber']}]: ${result['ErrorMessage']}`));
@@ -37,7 +37,24 @@ app.get('/api/raspan', async (req, res) => {
         })
         console.log(r.data);
     }
-    const navRes = await axios.get(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=6148&startDate=${year + "-" + month + "-" + date}`, { headers: headers })
+    const navRes = await axios.get(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=8873&startDate=${year + "-" + month + "-" + date}`, { headers: headers })
+    const navResult = navRes.data
+    if  (navResult['Success'] === false) {
+        console.log((`Error [${navResult['ErrorNumber']}]: ${navResult['ErrorMessage']}`));
+    }
+    if (navResult['TotalResults'] > 0) {
+        let nivotMsg = `Navigation: ${navResult['TotalResults']} Available dates!!`
+        nivotMsg += `\n${JSON.stringify(navResult['Results'])}`
+        console.log(nivotMsg);
+        const postData = await axios.post("https://api.pushover.net/1/messages.json", data = {
+            "token":   "ah2acbn5tnc7nhwb5umqgjju1k1m6z",
+	        "user":    "u85wt371dhctud6d7e29gkhanudcyu",
+            "message": nivotMsg
+        })
+        console.log(postData.data);
+    }
+
+    const navRes = await axios.get(`https://central.qnomy.com/CentralAPI/SearchAvailableDates?maxResults=31&serviceId=8873&startDate=${year + "-" + month + "-" + date}`, { headers: headers })
     const navResult = navRes.data
     if  (navResult['Success'] === false) {
         console.log((`Error [${navResult['ErrorNumber']}]: ${navResult['ErrorMessage']}`));
@@ -54,7 +71,6 @@ app.get('/api/raspan', async (req, res) => {
         console.log(postData.data);
     }
     
-
     console.log(result);
     return res.status(200).send({ "machine": result, "navigation": navResult})
     // axios.post(`https://${process.env.API_KEY}:${process.env.API_SECRET}@api.cloudinary.com/v1_1/${process.env.CLOUDNAME}/resources/search`, {"expression": "folder=bots/botimzozli"} ,{ headers: {'Access-Control-Allow-Origin' : '*'}})
